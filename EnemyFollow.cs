bf5f2e09df0457575c4657d0db28bcb43c5853ea
William Jones <wnj2f@mtmail.mtsu.edu>
2018-08-04 15:43:51 -0500

    Initial Commit

---
+++ EnemyFollow.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour 
+{
+
+	public float speed;
+	public float stoppingDistance;
+
+	private Transform target;
+
+	Rigidbody2D rb;
+
+	bool facingRight = true;
+
+	Vector3 localScale;
+
+	
+	void Start () 
+	{
+		localScale = transform.localScale;
+		rb = GetComponent<Rigidbody2D>();
+		target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+	}
+	
+	void Update()
+	{
+		if (Vector2.Distance(transform.position, target.position) > stoppingDistance)
+		{
+            transform.position = Vector2.MoveTowards(transform.position, new Vector2(target.position.x, transform.position.y), speed * Time.deltaTime);
+        }
+	}
+
+	void LateUpdate()
+	{
+		CheckWhereToFace();	
+	}
+
+	void CheckWhereToFace ()
+	{
+		// Check where player is relative to Enemy
+        if (target.position.x < transform.position.x)
+		{
+			facingRight = true;
+		}
+		else if (target.position.x > transform.position.x)
+		{
+			facingRight = false;
+		}
+
+		// Reverse Sprite
+		if (((facingRight) && (localScale.x > 0)) || ((!facingRight) && (localScale.x < 0)))
+		{
+			localScale.x *= -1;
+		}
+
+		transform.localScale = localScale;
+	}
+
+	void OnTriggerEnter2D (Collider2D col)
+	{
+		switch (col.tag) 
+		{
+			case "Platform":
+				rb.AddForce (Vector2.up * 1000f);
+				break;
+		}
+	}
+
+}
---
+++ PlayerAnim.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnim : MonoBehaviour
+{
+    private Animator anim;
+
+    void Start ()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update ()
+    {
+        // Walk Animation Condition
+        if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1)
+        {
+            anim.SetBool("isWalking", true);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+        }
+
+        // Jump Animation Condition
+        if (Input.GetButtonDown("Jump"))
+        {
+            anim.SetTrigger("jump");
+        }
+
+        // Weaponless Attack Animation Condition
+        if (Input.GetButtonDown("Fire1"))
+        {
+            anim.SetTrigger("attack");
+        }
+
+        /*
+        // Crouch Animation Condition
+        if (Input.GetButtonDown("Crouch"))
+        {
+            anim.SetBool("isCrouching", true);
+        }
+        else
+        {
+            anim.SetBool("isCrouching", false);
+        }
+        */
+    }
+}
